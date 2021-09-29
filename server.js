d14d510a1253f06ed510da70016cdfd05dce11c2
Chris Meagher <cpmeeg@gmail.com>
2021-09-29 19:54:12 +0100

    Update custom server impl
    This brings it more inline with current Next server impl.

--- server.js
+++ server.js
@@ -2,6 +2,51 @@ const next = require('next')
 const { createServer } = require('http')
 const appInsights = require('applicationinsights')
 
+// See https://github.com/vercel/next.js/blob/canary/packages/next/server/next.ts
+const getRequestHandler = (app, config) => {
+  return async (req, res, parsedUrl) => {
+    if (config.useAppInsights) {
+      appInsights.defaultClient.trackNodeHttpRequest({
+        request: req,
+        response: res
+      })
+    }
+
+    const nextRequestHandler = app.getRequestHandler()
+
+    return nextRequestHandler(req, res, parsedUrl)
+  }
+}
+
+// See https://github.com/vercel/next.js/blob/canary/packages/next/server/lib/start-server.ts
+const startServer = async (config) => {
+  const serverOptions = {
+    dev: config.env === 'development',
+    dir: '.',
+    quiet: false
+  }
+
+  const app = next(serverOptions)
+
+  const srv = createServer(getRequestHandler(app, config))
+
+  await new Promise((resolve, reject) => {
+    // This code catches EADDRINUSE error if the port is already in use
+    srv.on('error', reject)
+    srv.on('listening', () => resolve())
+    srv.listen(config.port, config.hostname)
+  })
+
+  // It's up to caller to run `app.prepare()`, so it can notify that the server
+  // is listening before starting any intensive operations.
+  const addr = srv.address()
+
+  return {
+    app,
+    actualPort: addr && typeof addr === 'object' ? addr.port : config.port
+  }
+}
+
 const initAppInsights = (instrumentationKey) => {
   if (!instrumentationKey) {
     return false
@@ -16,57 +61,44 @@ const initAppInsights = (instrumentationKey) => {
   return true
 }
 
-const startServer = async (config) => {
-  const serverOptions = {
-    dev: config.env === 'development',
-    dir: '.',
-    quiet: false
+const getPort = (defaultPort) => {
+  const envPort = process.env.PORT
+
+  if (typeof envPort === 'undefined') {
+    return defaultPort
   }
 
-  const app = next(serverOptions)
-  const handleNextRequests = app.getRequestHandler()
+  const parsedPort = parseInt(envPort, 10)
 
-  const srv = createServer((req, res) => {
-    if (config.useAppInsights) {
-      appInsights.defaultClient.trackNodeHttpRequest({
-        request: req,
-        response: res
-      })
-    }
+  return Number.isNaN(parsedPort) ? envPort : parsedPort
+}
 
-    handleNextRequests(req, res)
-  })
+const getEnv = (defaultEnv) => {
+  const env = process.env.APP_ENV || process.env.NODE_ENV
 
-  await new Promise((resolve, reject) => {
-    // This code catches EADDRINUSE error if the port is already in use
-    srv.on('error', reject)
-    srv.on('listening', () => resolve())
-    srv.listen(config.port, config.hostname)
-  })
+  if (typeof env !== 'undefined') {
+    return env
+  }
 
-  // It's up to caller to run `app.prepare()`, so it can notify that the server
-  // is listening before starting any intensive operations.
-  return app
+  return defaultEnv
 }
 
 const startTime = Date.now()
 
-const developmentEnv = 'development'
-
 const serverConfig = {
   hostname: 'localhost',
-  port: process.env.PORT || 3000,
-  env: process.env.APP_ENV || process.env.NODE_ENV || developmentEnv,
+  port: getPort(3000),
+  env: getEnv('development'),
   useAppInsights: initAppInsights(
     process.env.NEXT_PUBLIC_APPINSIGHTS_INSTRUMENTATIONKEY
   )
 }
 
 startServer(serverConfig)
-  .then(async (app) => {
+  .then(async ({ app, actualPort }) => {
     // eslint-disable-next-line no-console
     console.log(
-      `started server on host ${serverConfig.hostname}, port ${serverConfig.port}, env ${serverConfig.env}`
+      `started server on host ${serverConfig.hostname}, port ${actualPort}, env ${serverConfig.env}`
     )
 
     if (serverConfig.useAppInsights) {
