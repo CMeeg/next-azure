36bf699113ca0a35132706b6cbb75f46d1ee54bd
Chris Meagher <cpmeeg@gmail.com>
2023-08-19 10:51:24 +0100

    Reset to Create Next App v13 output

--- components/AppInsights/index.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { AppInsightsContext } from './ContextProvider'
-
-const useAppInsights = () => useContext(AppInsightsContext)
-
-export { useAppInsights }
--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-const compress = process.env.NEXT_COMPRESS ? !!process.env.NEXT_COMPRESS : true
-const assetPrefix = process.env.NEXT_PUBLIC_CDN_URL || ''
-const buildId = process.env.NEXT_PUBLIC_BUILD_ID || null
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = {
-  output: 'standalone',
-  httpAgentOptions: {
-    keepAlive: true,
-    maxSockets: 128,
-    maxFreeSockets: 128,
-    timeout: 60000
-  },
-  reactStrictMode: true,
-  poweredByHeader: false,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  compress,
-  assetPrefix,
-  generateBuildId: async () => buildId
-}
+module.exports = nextConfig
